c5c1a5fa200d8b81f52657cc7df0c90324bcc81e
Varun jha <jhavarun999@gmail.com>
2023-07-04 08:38:52 +0530

    first commit

---
+++ src/reducer/reducer.js
@@ -0,0 +1,16 @@
+const reducer = (state, action) => {
+
+    switch (action.type){
+        case  "GET_STORIES":{
+            return {
+                ...state,
+                hits: action.payload.hits,
+                nbPages: action.payload.nbPages
+            }
+        }
+    }
+
+    return state;
+}
+
+export {reducer}
